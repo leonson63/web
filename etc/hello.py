fd7060829acb176a25cf014d4eeb9eb1df8a2a0c
leonson63 <the_alexis@mail.ru>
2017-01-14 21:33:59 +0700

    Create hello.py

---
+++ etc/hello.py
@@ -0,0 +1,10 @@
+# config gunicorn
+bind = "127.0.0.1:8080"
+# или через сокет
+# bind = "unix:/home/proft/projects/blog/run/blog.socket"
+workers = 5
+# user = "www"
+# group = "www"
+# logfile = "/home/proft/projects/blog/log/gunicorn.log"
+# loglevel = "info"
+proc_name = "blog"
