9f55e45e735da244bb1a02c2682ae48de451b9d8
leonson63 <the_alexis@mail.ru>
2017-01-14 22:45:04 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@ sudo ln -s etc/hello.py /etc/gunicorn.d/hello.py
 # add nginx config
 sudo ln -s etc/nginx.conf /etc/nginx/conf.d/add.conf
 
-# run gunicorn
-#sudo gunicorn -v -с /etc/gunicorn.d/hello.py hello:wsgi
-sudo /etc/init.d/gunicorn restart
 # run nginx
 sudo /etc/init.d/nginx restart
+
+# run gunicorn
+sudo gunicorn -v -с /etc/gunicorn.d/hello.py hello:wsgi
+
