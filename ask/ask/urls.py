8f6a589f2bbf82810f0093d9d4d90f7bd42f6575
Alex <the_alexis@mail.ru>
2017-01-21 14:57:04 +0700

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^/', include('qa.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
