23973413c41695e9bd17d076fd30e19e01af06df
Alex <the_alexis@mail.ru>
2017-01-21 23:08:51 +0700

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -29,23 +29,23 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-#INSTALLED_APPS = (
-#    'django.contrib.admin',
+INSTALLED_APPS = (
+    'django.contrib.admin',
 #    'django.contrib.auth',
 #    'django.contrib.contenttypes',
 #    'django.contrib.sessions',
 #    'django.contrib.messages',
 #    'django.contrib.staticfiles',
-#)
+)
 
-#MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = (
 #    'django.contrib.sessions.middleware.SessionMiddleware',
-#    'django.middleware.common.CommonMiddleware',
-#    'django.middleware.csrf.CsrfViewMiddleware',
-#    'django.contrib.auth.middleware.AuthenticationMiddleware',
-#    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 #    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-#)
+)
 
 ROOT_URLCONF = 'ask.urls'
 
