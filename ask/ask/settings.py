6fe20b679f7cadc66cdac2de5025be9311d3a582
Alex <the_alexis@mail.ru>
2017-01-21 21:31:06 +0700

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '$$+(nel)q-_tpw2j(l$b%!fwl_1-m$)m58ki_k1=4s#-+c%$jx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
