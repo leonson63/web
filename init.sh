17ec6b4acf603b58d4ff743127b20009eed89cd9
leonson63 <the_alexis@mail.ru>
2017-01-21 13:03:48 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 # sudo ln -s etc/hello.py /etc/gunicorn.d/hello.py
 # add nginx config
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf etc/nginx.conf /etc/nginx/conf.d/add.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/conf.d/add.conf
 
 # run nginx
 sudo /etc/init.d/nginx restart
