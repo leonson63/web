1b1eae486bb2716a3b02003aa9dfe98e370984ab
Alex <the_alexis@mail.ru>
2017-01-25 22:45:07 +0700

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -16,14 +16,14 @@ class Question(models.Model):
   
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.OneToOneField(User)
     likes = models.ManyToManyField(User, related_name='question_like')
     
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(blank = True, auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.OneToOneField(User)
     
