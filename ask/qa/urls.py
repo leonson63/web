76ccac58df62600294e79ac5c6ffeab51d4b2237
Алеша <the_alexis@mail.ru>
2017-01-29 19:24:14 +0700

    updatet fo 2.5

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from qa.views import test
+from qa.views import show, test
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,9 +8,9 @@ urlpatterns = patterns('qa.views',
     url(r'^aaa/', include(admin.site.urls)),
     url(r'^login/', 'test', name='login'),
     url(r'^signup/', 'test', name='signup'),
-    url(r'^question/([^/]+)/', 'test', name='id'),
+    url(r'^question/(?P<id>\d+)/', 'test', name='id'),
     url(r'^ask/', test, name='ask'),
-    url(r'^popular/', test, name='popular'),
+    url(r'^popular/', show, name='popular'),
     url(r'^new/', test, name='new'),   
-    url(r'^$', test, name='all'),
+    url(r'^', show, name='all'),
 )
