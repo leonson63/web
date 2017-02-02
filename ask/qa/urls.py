e681df369522e3b27d1faf833d1353c781c196d9
Alex <the_alexis@mail.ru>
2017-02-02 20:49:36 +0700

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-from qa.views import all, popular, question, test, ask, login, signup
+from qa.views import all, popular, question, test, ask, tologin, signup
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('qa.views',
     url(r'^aaa/', include(admin.site.urls)),
-    url(r'^login/', login, name='login'),
+    url(r'^login/', tologin, name='login'),
     url(r'^signup/', signup, name='signup'),
     url(r'^question/(?P<id>\d+)/', question, name='question-id'),
     url(r'^ask/', ask, name='ask'),
